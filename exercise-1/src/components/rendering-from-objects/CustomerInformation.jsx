80164465e34a772b4b3fff4a66db1e8c1a3579e0
Ati abbasi <atiabaci8183@gmail.com>
2025-05-27 08:44:45 +0330

    homework done

---
+++ exercise-1/src/components/rendering-from-objects/CustomerInformation.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CustomerInformation({ first_name, last_name }) {
+  return (
+    <div>
+      <h1>My name is {first_name}</h1>
+      <h2>My last name is {last_name}</h2>
+    </div>
+  );
+}
